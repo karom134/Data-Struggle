284e718a66148ee8439fb134a0967d16388da623
karom-V <karom.voa@student.mahidol.edu>
2019-11-04 19:33:37 +0700

    proper Last.java

--- a6/Last.java
+++ a6/Last.java
@@ -1,40 +1,34 @@
 public class Last{
     public static Integer binarySearchLast(int[] a, int k){
-        if(a[0]>k||a[a.length-1]<k){
-            return null;
-        }
-        if (a[a.length-1]==k){
-            return a.length-1;
-        }
-        int ans=binary_search_helper(a, k, 0, a.length);
-        if(a[ans]!=k){
-            return null;
-        }
-        return ans;
+        return binary_search_helper(a, k, 0, a.length,false);
     }
-    public static Integer binary_search_helper(int[] a,int k, int l, int r){
+    public static Integer binary_search_helper(int[] a,int k, int l, int r,boolean x){
         int m = (l+r)/2;
         if(r-l<=1){
+            if(!x&&a[m]!=k){
+                return null;
+            }
             return m;
         }
         else{
             if(a[m]>k){
-                return binary_search_helper(a, k, l, m-1);
+                return binary_search_helper(a, k, l, m-1,x);
             }
             else if(a[m]==k){
-                return binary_search_helper(a, k, m, r);
+                x=true;
+                return binary_search_helper(a, k, m, r,x);
             }
             else{
-                return binary_search_helper(a, k, m+1, r);
+                return binary_search_helper(a, k, m+1, r,x);
             }
         }
     }
 
     public static void main(String arg[]){
         System.out.println(binarySearchLast(new int[]{2, 2, 2, 2, 2, 2,2,2,2,2,2,2,2,2,2,3}, 2));
-        System.out.println(binarySearchLast(new int[]{1, 2, 2, 2, 4,4, 5}, 1));
+        System.out.println(binarySearchLast(new int[]{1, 2, 2, 2, 4,4, 5}, 0));
         System.out.println(binarySearchLast(new int[]{1,1, 2, 2, 2, 4,4, 5}, 2));
-        System.out.println(binarySearchLast(new int[]{1,1, 2, 2, 2, 4,4, 5}, 4));
+        System.out.println(binarySearchLast(new int[]{1,1, 2, 2, 2, 4,4, 5}, 3));
         System.out.println(binarySearchLast(new int[]{1,1, 2, 2, 2,3, 4,5, 5}, 5));
     }
 }
